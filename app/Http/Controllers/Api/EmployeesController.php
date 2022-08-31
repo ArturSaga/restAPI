c4cf01c76fd2964c14d77165cf5d6588961d9973
mac <asagataev@list.ru>
2022-08-31 19:45:59 +0700

    First commit

---
+++ app/Http/Controllers/Api/EmployeesController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Employees;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class EmployeesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $employees = Employees::all();
+
+        return json_decode($employees);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'email'],
+            'fullName' => ['required'],
+            'age' => ['required'],
+            'experience' => ['required'],
+            'photo' => ['sometimes'],
+            'averageSalary' => ['required', 'integer']
+        ]);
+        if($validator->fails()){
+            return json_decode($validator->errors());
+        }
+
+        $employee = Employees::create($request->all());
+        return json_decode($employee);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $employee = Employees::find($id);
+
+        return json_decode($employee);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
